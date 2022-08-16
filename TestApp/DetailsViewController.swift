5f1c6ef96933f1f154951f84728c12b46ac7a3f1
Oksenoyt <a.oksenoyt@gmail.com>
2022-08-16 17:52:57 +0300

    0.1

--- TestApp/DetailsViewController.swift
+++ TestApp/DetailsViewController.swift
@@ -16,7 +16,25 @@ class DetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        characterPhoto.image = UIImage(named: character.image)
-        textDetails.text = character.name
+//        characterPhoto.image = UIImage(named: character.image)
+//        textDetails.text = character.name
+        fetchImage()
     }
+    
+    private func fetchImage() {
+        guard let url = URL(string: "www.com") else { return }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            guard let data = data, let response = response else {
+                print(error?.localizedDescription ?? "нихрена нет ошибок")
+                return
+            }
+            
+            print(response)
+            
+            guard let image = UIImage(data: data) else { return }
+            self?.characterPhoto.image = image
+        }.resume()
+    }
+    
 }
